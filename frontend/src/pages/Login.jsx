0f7d0dff81aa96f605e13c207f585adbd35dd1ed
amolkadam5256 <amolkadam1274@gmail.com>
2025-01-07 22:16:01 +0530

    first commit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -16,29 +16,30 @@ const Login = () => {
   return (
     <>
       <form action="" className='min-h-[80vh] flex items-center'>
-        <div>
-          <p>{state == 'Sign Up' ? "Create Account" : 'Login'}</p>
+        <div className='flex flex-col gap-3 m-auto item-start p-8 min-w-[340px] sm-min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg '>
+          <p className='text-2xl font-semibold '>{state == 'Sign Up' ? "Create Account" : 'Login'}</p>
 
           <p>Please {state == 'Sign Up' ? "Sign Up" : 'Log in'}  to book appointment</p>
 
-          <div>
+          <div className='w-full'>
             <p>Full Name</p>
             <input type="text" required value={name} placeholder='Enter Full Name' onChange={(e) => setName(e.target.value)} />
 
           </div>
 
 
-          <div>
+          <div className='w-full'>
             <p>Email</p>
             <input type="email" required value={name} placeholder='Enter Email Address' onChange={(e) => setEmail(e.target.value)} />
           </div>
 
-          <div>
+          <div className='w-full'>
             <p>Password</p>
             <input type="Password" required value={name} placeholder='Enter Password ' onChange={(e) => setPassword(e.target.value)} />
 
           </div>
 
+          <button className='text-start'>{state == 'Sign Up' ? "Create Account" : 'Login'}</button>
         </div>
 
       </form>
